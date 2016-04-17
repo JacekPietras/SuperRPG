ac03c493832ff8666658c524520a12f1551e5547
Jacek Pietras <jock69pl@gmail.com>
2016-04-17 22:31:53 +0200

    Init

---
+++ html/src/com/jock/rpg/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jock.rpg.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jock.rpg.SuperRPG;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SuperRPG();
+        }
+}
